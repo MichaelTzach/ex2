9e4358b14e973525e357328024debfa4ac09cb9a
Michael Tzach <michaeltzach@gmail.com>
2017-12-02 20:49:45 +0200

    After second attempt. before writing the report

--- ex2.R
+++ ex2.R
@@ -5,32 +5,40 @@ setwd(workspaceDir)
 
 #turn na strings to empty strings to avoid issues when creating the model
 #also, avoid turingn strings to factor automatically
-trainingData <- read.csv("train.csv", na.string="")
-trainingData$Survived = factor(trainingData$Survived)
-trainingData$Pclass = factor(trainingData$Pclass)
+mapNamesToTitles = function(y) {
+  namesAsChars = lapply(y, as.character)
+  firstPartOfNamesBeforeDots = lapply(namesAsChars, function(x) strsplit(x, '.', fixed = TRUE)[[1]][[1]])
+  titlesPartOfNamesAsList = lapply(firstPartOfNamesBeforeDots, function(x) strsplit(x, ', ', fixed = TRUE)[[1]][[2]])
+  #titlesPartOfNamesAsList = lapply(titlesPartOfNamesAsList, function(x) if (x == "Dona") NA else x)
+  return(factor(unlist(titlesPartOfNamesAsList)))
+}
 
-#head(trainingData)
+preprocessData = function(data, isTestData) {
+  data$Pclass = factor(data$Pclass)
+  data$Title = mapNamesToTitles(data$Name)
+  if (isTestData) {
+    data = subset(data, select=-c(Ticket, Cabin, Name))  
+  } else {
+    data$Survived = factor(data$Survived)
+    data = subset(data, select=-c(Ticket, Cabin, PassengerId, Name))  
+  }
+}
 
-#remove unwanted features from the data frame
-trainingData = subset(trainingData, select=-c(Name, Ticket, Cabin, PassengerId))
+dataFromFile = function(fileName, isTestData) {
+  data = read.csv(fileName, na.string="")
+  preprocessedData = preprocessData(data, isTestData)
+  return(preprocessedData)
+}
 
-### Read and preprocess the training data
-#turn na strings to empty strings to avoid issues when creating the model
-#also, avoid turingn strings to factor automatically
-testData <- read.csv("test.csv", na.string="")
-testData$Survived = factor(testData$Survived)
-testData$Pclass = factor(testData$Pclass)
-
-#head(testData)
-
-#remove unwanted features from the data frame
+trainingData = dataFromFile("train.csv", FALSE)
+testData = dataFromFile("test.csv", TRUE)
 testDataPassengerIds<- testData$PassengerId
-testData = subset(testData, select=-c(Name, Ticket, Cabin, PassengerId))
+testData = subset(testData, select=-c(PassengerId))
 
 ##Divide the training data to a training part and evaluation part
-indices <- sample(1:nrow(trainingData),nrow(trainingData)*0.75)
-trainingData.train<- trainingData[indices,]
-trainingData.test<- trainingData[-indices,]
+#indices <- sample(1:nrow(trainingData),nrow(trainingData)*0.75)
+#trainingData.train<- trainingData[indices,]
+#trainingData.test<- trainingData[-indices,]
 
 #install C50 and add the lib
 if(!require(C50)){
@@ -39,19 +47,51 @@ if(!require(C50)){
 library('C50')
 set.seed(123)
 
+#Print some data on things
+trainingData.Cols<- 1:dim(trainingData)[2]
+trainingData.factors <- trainingData.Cols[sapply(trainingData,is.factor)]
+trainingData.numerics <- trainingData.Cols[!sapply(trainingData,is.factor)]
+if(!require(tidyr)) {
+  install.packages("tidyr")
+}
+library(tidyr)
+#tidy data
+trainingData.tidy_factors<-gather(trainingData[,trainingData.factors],"feature","value",-1)
+trainingData.tidy_numerics<-gather(cbind(Survived=trainingData[,1],trainingData[,trainingData.numerics]),"feature","value",-1)
+if(!require(ggplot2)) {
+  install.packages("ggplot2")
+}
+library(ggplot2)
+qplot(x=value,data=trainingData.tidy_factors,fill=Survived) + facet_grid(~feature,scales="free")
+qplot(x=value,data=trainingData.tidy_numerics,fill=Survived) + facet_grid(~feature,scales="free")
+
+if(!require(caret)) {
+  install.packages("caret")
+}
+library(caret)
+if(!require(e1071)) {
+  install.packages("e1071")
+}
+library(e1071)
+
+control <- trainControl(method="cv", number=10)
+fit.c50 <- train(Survived~., data=trainingData, method="C5.0", metric="Accuracy", trControl=control,na.action = na.pass)
+fit.c50$xlevels[["Title"]] <- union(fit.c50$xlevels[["Title"]], levels(testData$Cabin))
+
+testPredictions = predict(fit.c50,testData,na.action = na.pass)
 #Use C50 and predict on the test part
-C50training <-C5.0(Survived ~., data=trainingData.train )
+#C50training <-C5.0(Survived ~., data=trainingData.train )
 #plot(C50training)
 
-trainingPredictions = predict(C50training, trainingData.test)
-table(trainingPredictions,trainingData.test$Survived)
-mean(trainingPredictions==trainingData.test$Survived)
+#trainingPredictions = predict(C50training, trainingData.test)
+#table(trainingPredictions,trainingData.test$Survived)
+#mean(trainingPredictions==trainingData.test$Survived)
 
 #Create a prediction on the full training data and run it on the test data
-C50 <-C5.0(Survived ~., data=trainingData )
-plot(C50training)
-testPredictions = predict(C50training, testData)
+#C50 <-C5.0(Survived ~., data=trainingData )
+#plot(C50)
+#testPredictions = predict(C50training, testData)
 
 #Create the result of the prediction
 res <- cbind(PassengerId=testDataPassengerIds,Survived=as.character(testPredictions))
-write.csv(res,file="try1.csv",row.names = F)
+write.csv(res,file="try2.csv",row.names = F)
